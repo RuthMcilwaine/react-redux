9e8a4c8e09d8615511c0e0ea558c7636111ab083
Ruth Mcilwaine <ruth.mcilwaine@myob.com>
2023-06-25 21:02:02 +1000

    persisted the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { todos } from '../components/reducers.js'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+import { todos } from "../components/reducers.js";
+
+const reducers = {
+  todos: todos,
+};
+
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers(reducers);
+const perisitedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    todos: todos,
-  },
+  reducer: perisitedReducer,
 });
 
 export default store;
+
+//To ensure that the state of our app is kept through a browser refresh, we'll be using a package called redux persist.
+//wrap our rootReducer using the persistReducer
+//'persistConfig' is an object that tells Redux Persist how to save and where to store our app's data
+//imported 'storage' defaults to local storage on the web
+//'stateReconciler' property that we set to auto merge to level two. The autoMergeLevel2 tells Redux Persist how to reconcile(*consistent) the initial and stored states of our app as in how deep should it go when doing so.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App.js';
+import React from "react";
+//import { configureStore } from "@reduxjs/toolkit";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import App from "./App.js";
 
-createRoot(document.getElementById('root')).render(
+//const store = configureStore();
+const persistor = persistStore(store);
+
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
