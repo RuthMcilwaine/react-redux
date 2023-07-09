0652e0ac0c4a0e392f4c04ebe8662e8b1b35f2cd
Ruth Mcilwaine <Ruth.mcilwaine@myob.com>
2023-07-10 09:33:13 +1000

    reverted persisting the store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+//import { increment, incrementAsync, incrementIfOdd } from "./components/thunks";
 import React from "react";
-//import { configureStore } from "@reduxjs/toolkit";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./App.js";
 
-//const store = configureStore();
-const persistor = persistStore(store);
-
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={<div>loading...</div>} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>
 );
+
+// store.dispatch(increment());
+// store.dispatch(incrementAsync());
+// store.dispatch(incrementIfOdd());
