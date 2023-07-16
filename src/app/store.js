788a288b1bf23947561e12e56ad03375a8c87855
Ruth Mcilwaine <Ruth.mcilwaine@myob.com>
2023-07-16 17:22:30 +1000

    add redux-thunk dependencies

--- src/app/store.js
+++ src/app/store.js
@@ -10,16 +10,7 @@ const middleware = [thunk];
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => {
-    const customMiddleware = getDefaultMiddleware().concat(...middleware);
-    return customMiddleware;
-  },
+  middleware: middleware,
 });
 
 export default store;
-
-//the state of the app is stored after a browser refresh, use redux persist.
-//wrap the rootReducer using the persistReducer
-//'persistConfig' is an object that tells Redux Persist how to save and where to store our app's data
-//imported 'storage' defaults to local storage on the web
-//'stateReconciler' property that we set to auto merge to level two. The autoMergeLevel2 tells Redux Persist how to reconcile(*consistent) the initial and stored states of our app as in how deep should it go when doing so.
