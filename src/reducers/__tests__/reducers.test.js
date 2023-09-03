46f28619a81fa0f119b1dd8be3cf9382fccbfcfe
Ruth Mcilwaine <Ruth.mcilwaine@myob.com>
2023-09-03 16:45:13 +1000

    completed todo unit test

--- src/reducers/__tests__/reducers.test.js
+++ src/reducers/__tests__/reducers.test.js
@@ -1,4 +1,8 @@
-import { CREATE_TODO, REMOVE_TODO } from "../../components/actionTypes";
+import {
+  COMPLETED_TODO,
+  CREATE_TODO,
+  REMOVE_TODO,
+} from "../../components/actionTypes";
 import todosReducer from "../reducers";
 
 describe("todosReducer", () => {
@@ -33,6 +37,27 @@ describe("todosReducer", () => {
       expect(nextState.todos.some((todo) => todo.text === "Shopping")).toBe(
         false
       );
+    });
+  });
+
+  describe("COMPLETED_TODO", () => {
+    it("should mark todo as completed", () => {
+      const state = {
+        loading: false,
+        todos: [{ text: "Shopping", isCompleted: false }],
+      };
+      const action = {
+        type: COMPLETED_TODO,
+        payload: { text: "Shopping" },
+      };
+      const nextState = todosReducer(state, action);
+      console.log(nextState.todos);
+
+      const completedTodo = nextState.todos.find(
+        (todo) => todo.text === "Shopping"
+      );
+      console.log(completedTodo);
+      expect(completedTodo.isCompleted).toBe(true);
     });
   });
 });
