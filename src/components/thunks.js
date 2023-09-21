30ee0844fc589efe92bfc814a8459ac8c9bc8c32
Ruth Mcilwaine <Ruth.mcilwaine@myob.com>
2023-09-21 21:01:20 +1000

    made API request to fetch todos from localhost

--- src/components/thunks.js
+++ src/components/thunks.js
@@ -3,14 +3,13 @@ import {
   fetchTodosFailure,
   fetchTodosRequest,
 } from "../components/actionTypes";
-import todosResponse from "../mappings/data/loadTodosResponse.json";
+//import todosResponse from "../mappings/data/loadTodosResponse.json";
 
 export const fetchTodos = () => async (dispatch) => {
   dispatch(fetchTodosRequest());
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
+    const todosResponse = await fetch("http://localhost:5000/todos");
     dispatch(fetchTodosSuccess(todosResponse));
   } catch (error) {
     dispatch(fetchTodosFailure(error));
